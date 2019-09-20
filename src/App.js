b88fad952441609b2107311928cf9f8ed7eed79c
unknown <neduvinci@gmail.com>
2019-09-20 08:57:07 +0100

    Fetched data using axios and set it to state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header.js";
+import Axios from "axios";
 
 
 export default function App() {
+
+  const [charactersState, setCharacters] = useState([]);
+  console.log(charactersState);
+
+  useEffect(() => {
+
+    Axios.get('https://rickandmortyapi.com/api/character')
+      .then(yesPlease => {
+        setCharacters(yesPlease.data.results);
+      // debugger
+      })
+      .catch(hellNo => {
+        
+      })
+    
+    }
+    , []
+  );
+
   return (
     <main>
       <Header />
