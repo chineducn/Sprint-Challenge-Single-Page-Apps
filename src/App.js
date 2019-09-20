53d86e025e04f549b1f5bec504f266b0137be118
unknown <neduvinci@gmail.com>
2019-09-20 13:55:37 +0100

    Hooked up Welcome Page and CharacterList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import Header from "./components/Header.js";
-import CharacterList from './components/CharacterList'
-import Axios from "axios";
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+import { Route, NavLink } from 'react-router-dom';
+import tailored from 'styled-components';
 
+const CoolNav = tailored.nav`
+  display: flex;
+  justify-content: space-around;
+`
 
 export default function App() {
 
-  
+
   return (
     <main>
       <Header />
-      <CharacterList />
+      <CoolNav>
+        <NavLink to='/'>Welcome</NavLink>
+        <NavLink to='/characters'>Characters</NavLink>        
+      </CoolNav>
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterList} />
     </main>
   );
 }
