ad79f93c54d499f2ef6d2eb2b28e874630e09b3c
unknown <neduvinci@gmail.com>
2019-09-20 09:20:08 +0100

    Created CharacterCard component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
+import tailored from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+  const { name, gender, species, url } = props;
+
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{gender}</p>
+      <p>{species}</p>
+      <p>{url}</p>
+    </div>);
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
-
+  const { charactersArray } = props
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
